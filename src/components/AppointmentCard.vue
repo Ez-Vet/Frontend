1b99c42b0dd87fc0727dd2be1517cc82992d78d0
ghostfound404 <dbbastidas08@gmail.com>
2024-09-24 19:11:24 -0500

    Citas

---
+++ src/components/AppointmentCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="appointment-card">
+      <h3>Consulta Veterinaria</h3>
+      <p><strong>Fecha:</strong> {{ date }}</p>
+      <p><strong>Hora:</strong> {{ time }}</p>
+      <p><strong>Doctor:</strong> {{ doctor }}</p>
+      <p><strong>Mascota:</strong> {{ pet }}</p>
+      <p><strong>Descripción:</strong> {{ description }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'appointmentCard',
+    props: {
+      date: String,
+      time: String,
+      doctor: String,
+      pet: String,
+      description: String
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .appointment-card {
+    background-color: white;
+    padding: 20px;
+    margin-bottom: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  }
+  </style>
+  
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+      <button class="btn">Agendar Una Cita</button>
+      <div class="bell-icon">
+        <i class="fas fa-bell"></i>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header',
+  };
+  </script>
+  
+  <style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    background-color: #8ed8d8;
+  }
+  
+  .btn {
+    padding: 10px 20px;
+    background-color: #333;
+    color: white;
+    border: none;
+    border-radius: 5px;
+  }
+  
+  .bell-icon {
+    background-color: #f1f1f1;
+    padding: 10px;
+    border-radius: 50%;
+  }
+  </style>
+  
