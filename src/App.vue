e29eb135afdf7a638dc63cb7f288c462ddf85853
sebasepe <u202125968@upc.edu.pe>
2024-09-25 02:05:41 -0500

    fe

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <template>
-    <div id="app">
-      <Sidebar />
-      <MainContent />
-      <NotificationBell />
+  <div class="app-container">
+    <sidebar />
+    <div class="main-content">
+      <header />
+      <Appointments />
     </div>
-  </template>
-  
-  <script>
-  import Sidebar from './components/Sidebar.componet.vue'
-  import MainContent from './components/MainContent.component.vue'
-  import NotificationBell from './components/NotificationBell.component.vue'
-  
-  export default {
-    name: 'App',
-    components: {
-      Sidebar,
-      MainContent,
-      NotificationBell
-    }
-  }
-  </script>
-  
-  <style>
-  #app {
-    display: flex;
-    position: relative;
-  }
-  
-  .notification-bell {
-    position: absolute;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+import Header from './components/Header.vue';
+import Appointments from './components/Appointments.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Header,
+    Appointments,
+  },
+};
+</script>
+
+<style>
+.app-container {
+  display: flex;
+}
+
+.main-content {
+  flex-grow: 1;
+  background-color: #f5f5f5;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import '@fortawesome/fontawesome-free/css/all.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import i18n from './i18n';
+import './style.css';
 
 const app = createApp(App);
 
+app.use(i18n); // Usar i18n en la aplicación
 
 app.mount('#app');
+createApp(App).mount('#app');
