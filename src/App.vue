f47c618aa936f360b3342ccba361c749eca65b9e
ghostfound404 <dbbastidas08@gmail.com>
2024-09-25 01:35:33 -0500

    feat:update.appointments

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 <template>
-    <div class="app-container">
-      <sidebar />
-      <div class="main-content">
-        <header />
-        <Appointments />
-      </div>
+  <div class="app-container">
+    <sidebar />
+    <div class="main-content">
+      <header />
+      <Appointments />
     </div>
-  </template>
-  
-  <script>
-  import Sidebar from './components/Sidebar.vue';
-  import Header from './components/Header.vue';
-  import Appointments from './components/Appointments.vue';
-  
-  export default {
-    name: 'App',
-    components: {
-      Sidebar,
-      Header,
-      Appointments,
-    },
-  };
-  </script>
-  
-  <style>
-  .app-container {
-    display: flex;
-  }
-  
-  .main-content {
-    flex-grow: 1;
-    background-color: #f5f5f5;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+import Header from './components/Header.vue';
+import Appointments from './components/Appointments.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Header,
+    Appointments,
+  },
+};
+</script>
+
+<style>
+.app-container {
+  display: flex;
+}
+
+.main-content {
+  flex-grow: 1;
+  background-color: #f5f5f5;
+}
+</style>
